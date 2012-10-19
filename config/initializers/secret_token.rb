3199aff4c0f76edd9a9acf349b64a7648a55e5fa
hypersleep <hypersleep@hypersleep-X101.(none)>
2012-10-19 19:12:35 +0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Medinst::Application.config.secret_token = 'dc53fdeea6d6c9610b194572c55ef6a7621f198279e8748656baa0efaa92543a3c6df8383e663585460c4f1a626bbb164b994536fe1dc6ea612c6b7834b1e52f'
